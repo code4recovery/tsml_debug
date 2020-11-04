36dfcdc9b2903a276f1b0c6cae5194a5f9cedfa5
Brian Webster <bwebster@iastate.edu>
2020-11-03 23:24:27 -0600

    Initial Checkin - Create TSML Debug plugin

---
+++ tsml-debug.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: TSML Debug
+Plugin URI: https://wordpress.org/plugins/tsml-debug
+Description: Provide debug/support services for the 12 Step Meeting List Plugin
+Version: 0.0.0
+Author: Code4Recovery
+Author URI: https://github.com/code4recovery/tsml-debug
+Text Domain: tsml-debug
+ */
