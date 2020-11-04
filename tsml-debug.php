cb4b7e5e60d80d62ff350570726da557117e383a
Brian Webster <bwebster@iastate.edu>
2020-11-04 09:18:12 -0600

    Added debug to the Admin Menu

--- tsml-debug.php
+++ tsml-debug.php
@@ -8,3 +8,16 @@ Author: Code4Recovery
 Author URI: https://github.com/code4recovery/tsml-debug
 Text Domain: tsml-debug
  */
+
+ /* Ensure 12 Step Meeting List Plugin is enabled */
+ if (defined('TSML_VERSION')) {
+     /* Add Admin Menu item */
+     if (is_admin()) {
+        add_action('admin_menu', 'tsml_debug_admin_menu');
+
+        function tsml_debug_admin_menu() {
+            add_submenu_page('edit.php?post_type=tsml_meeting', __('TSML Debug', 'tsml-debug'), __('TSML Debug', 'tsml-debug'), 'manage_options', 'tsml_debug', 'tsml_debug_page');
+        }
+     }
+
+ }
